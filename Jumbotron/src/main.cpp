669d3ba1f045c4b2dcfc0752c71c849b24f6340f
Owen <77owen.b@gmail.com>
2022-10-04 19:31:20 -0600

    connec to dashboard with qr code

--- Jumbotron/src/main.cpp
+++ Jumbotron/src/main.cpp
@@ -5,6 +5,8 @@
 #include <qrcode_espi.h>
 //#include <lvgl.h>
 
+int run = 0;
+
 TFT_eSPI tft = TFT_eSPI();
 uint16_t x, y;
 QRcode_eSPI qrcode (&tft);
@@ -18,7 +20,7 @@ Card statusCard(&dashboard, STATUS_CARD, "Status", "success");
 void setup() {
 
 statusCard.update("Test");
-WiFi.softAP("Jumbotron");
+WiFi.softAP("Jumbotron", "password", 12, 1);
 server.begin();
 
 
@@ -28,16 +30,36 @@ server.begin();
   tft.fillScreen(TFT_BLACK);
 
   qrcode.init();
-  qrcode.create("https://www.curobotics.net/");
+  qrcode.create("WIFI:S:Jumbotron;T:WPA;P:password;;");
+  tft.setRotation(0);
+  tft.drawString("Connect", 5, 5, 4);
+
 
 }
   
 void loop() {
 
   if(tft.getTouch(&x, &y)) {
+    if(run == 0) {
+      run++;
+      tft.fillScreen(TFT_BLACK);
+      tft.setRotation(1);
+      qrcode.create("http://192.168.4.1");
+      tft.setRotation(0);
+      tft.drawString("Dashboard", 5, 5, 4);
+      delay(500);
+    }else if (run == 1) {
+      tft.setRotation(1);
+      run++;
+      tft.fillScreen(TFT_BLACK);
+    }
+    else{
+      
     Serial.print("X = "); Serial.print(x);
     Serial.print(", Y = "); Serial.println(y);
     tft.drawString("X: "+String(x)+"  Y: "+String(y)+"                ", 10, 10, 4);
+    }
+    
   }
 
 }
